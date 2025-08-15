336782dc083a118890f915bd1143e62524be0505
powerlet2020 <powerlet2020@gmail.com>
2025-08-15 02:11:30 -0500

    create multiple pages using react router dom

--- ecommerce-store/src/App.jsx
+++ ecommerce-store/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomePage from './Pages/HomePage'
+import { Routes, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <HomePage/>
+        <Routes>
+          <Route index element={<HomePage />} /> 
+          <Route path="/checkout" element={<div>CheckOut</div>} />
+        </Routes>
     </div>
   )
 }
